537571e3ecb296b4d866cff58bdcf6ae53447dc4
Abdur Raheem Abdus Samad Larry <samadlarry7@gmail.com>
2022-06-19 10:25:43 +0000

    made a change on the models

--- I4G001213IDI/blog/models.py
+++ I4G001213IDI/blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 class Post(models.Model):
     title   = models.CharField(max_length=200)
     text    = models.TextField
-    author  = models.ForeignKey(get_user_model(),)
+    author  = models.ForeignKey(get_user_model())
     created_date    = models.DateTimeField
     published_date  = models.DateTimeField
     
